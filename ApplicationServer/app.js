c256b251546271fba4cefa287256a8d7c268345c
aayu95 <aayu95@gmail.com>
2020-05-08 16:55:34 +1000

    Hosted UI  on server

--- ApplicationServer/app.js
+++ ApplicationServer/app.js
@@ -7,11 +7,17 @@ const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', function (req, res) {
-    res.send('Working!');
-})
+
+app.use(express.static('../UI/',{root:'../'}));
+
+app.get('*', function(req, res){
+  res.sendFile('/UI/index.html',{root:'../'});
+});
+
+
 
 require('./routes/couchDB.routes')(app);
+
 
 app.listen(3000, () => {
     console.log("Server is running")
