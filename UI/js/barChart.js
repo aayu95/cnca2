4e9a25b8cf6b6f44c02b1db585ce9eebbf4d2758
anoushkadoctor <akdoctor@gmail.com>
2020-05-16 14:25:40 +1000

    Live data streaming added

---
+++ UI/js/barChart.js
@@ -0,0 +1,85 @@
+var margin = {top: 20, right: 20, bottom: 80, left: 70},
+            width = 600 - margin.left - margin.right,
+            height = 350 - margin.top - margin.bottom;
+
+            var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+            var y = d3.scale.linear().range([height, 0]);
+
+            var xAxis = d3.svg.axis()
+                .scale(x)
+                .orient("bottom")
+                .ticks(10);
+
+            var yAxis = d3.svg.axis()
+                .scale(y)
+                .orient("left")
+                .ticks(10);
+
+            var tip = d3.tip()
+                .attr('class', 'd3-tip')
+                .offset([-10, 0])
+                .html(function(d) {
+                  return "<strong>Tweets:</strong> <span style='color:black;'>" + d.num_tweets + "</span>";
+                })
+
+            var svg = d3.select("#barGraph").append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", 
+                    "translate(" + margin.left + "," + margin.top + ")");
+            
+            svg.call(tip);
+
+            d3.json("dummy_twitter.json", function(error, data) {
+                
+            x.domain(data.map(function(d) { return d.name; }));
+            y.domain([0, d3.max(data, function(d) { return d.num_tweets; })]);
+
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis)
+                .selectAll("text")
+                .style("text-anchor", "end")
+                .attr("dx", "-.8em")
+                .attr("dy", "-.55em")
+                .attr("transform", "rotate(-35)"); //rotates the axis label
+
+                svg.append("text")   
+                .attr("class", "x_axis")          
+                .attr("transform",
+                        "translate(" + (width/2) + " ," + 
+                                    (height + margin.top + 45) + ")")
+                .style("text-anchor", "middle")
+                .text("Suburb");
+
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em");
+
+                svg.append("text")
+                .attr("class", "y_axis")         
+                .attr("transform", "rotate(-90)")
+                .attr("y", 0 - margin.left + 10)
+                .attr("x",0 - (height/2))
+                .attr("dy", "1em")
+                .style("text-anchor", "middle")
+                .text("Number of Tweets");  
+
+            svg.selectAll(".bar")
+                .data(data)
+                .enter().append("rect")
+                .attr("class", "bar")
+                .attr("x", function(d) { return x(d.name); })
+                .attr("width", x.rangeBand())
+                .attr("y", function(d) { return y(d.num_tweets); })
+                .attr("height", function(d) { return height - y(d.num_tweets);})
+                .on('mouseover', tip.show)
+                .on('mouseout', tip.hide);
+            });
--- UI/js/pageDisplay.js
+++ UI/js/pageDisplay.js
@@ -29,4 +29,50 @@ $(document).ready(function() {
         $("#about").hide();
         $("#barGraph").show();
     });
+
+    //Create table from datastream
+    var data;
+    $.getJSON('dummy_stream.json', function(result){
+        data=result;
+    });
+    var id=0;
+    
+    window.setTimeout(function() {
+        var table = '<table border="1" id="tweetData"><thead><tr><th style="padding: 6px;">ID</th><th style="padding: 6px;">Tweet</th><th style="padding: 6px;">Sentiment</th><th style="padding: 6px;">Location</th></tr></thead><tbody>';
+        for(var i = id; i<(id+5); i++){
+            table+='<tr><td style="padding: 6px;">';
+            table+=data[i].id;
+            table+='</td><td style="padding: 6px;">';
+            table+=data[i].tweet_data;
+            table+='</td><td style="padding: 6px;">';
+            table+=data[i].sentiment;
+            table+='</td><td style="padding: 6px;">';
+            table+=data[i].location;
+            table+='</td></tr>';
+        }
+        id+=5;
+        table+='</tbody></table>';
+        $("#dataStream").html(table);
+    }, 10);
+
+
+    window.setInterval(function() {
+        var table = '<table border="1" id="tweetData"><thead><tr><th style="padding: 6px;">ID</th><th style="padding: 6px;">Tweet</th><th style="padding: 6px;">Sentiment</th><th style="padding: 6px;">Location</th></tr></thead><tbody>';
+        for(var i = id; i<(id+5); i++){
+            table+='<tr><td style="padding: 6px;">';
+            table+=data[i].id;
+            table+='</td><td style="padding: 6px;">';
+            table+=data[i].tweet_data;
+            table+='</td><td style="padding: 6px;">';
+            table+=data[i].sentiment;
+            table+='</td><td style="padding: 6px;">';
+            table+=data[i].location;
+            table+='</td></tr>';
+        }
+        id+=5;
+        if(id>=15){id=0;}
+        table+='</tbody></table>';
+        $("#dataStream").html(table);
+    }, 5000);
+
 });
