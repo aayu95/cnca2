4d9c3a7e0a15110256bbce89ee3cbafa4a7bfdcd
Atif <ma_atif@hotmail.com>
2020-05-20 22:33:36 +1000

    Added code to assign suburb name to tweets from geo coordinates

--- Database/load_twitter_dump.py
+++ Database/load_twitter_dump.py
@@ -17,7 +17,35 @@ databaseServer = couchdb.Server('http://admin:admin@127.0.0.1:5984/')
 # Create database
 #database = databaseServer.create('twitter_melb_dump') 
 database = databaseServer['twitter_melb_dump']
-			
+
+global geodict
+geodict = {}
+# Load melbourne geo json file to map tweet cordinates to Melbourne Suburbs
+with open('Data/melbourne.geojson') as melbournejson:
+	data = json.load(melbournejson)
+
+	for p in data['features']:
+		id = p['id']
+		name = p['properties']['SA2_NAME16']
+		geodict[id] = {}
+		geodict[id]['name'] = name
+		if p['geometry']['type'] == 'Polygon':
+			geodict[id]['type'] = 'polygon'
+			coorlists = p['geometry']['coordinates'][0]
+			polygonlist = []
+			for coor in coorlists:
+				polygonlist.append((coor[0],coor[1]))
+			polygon = Polygon(polygonlist)
+			geodict[id]['polygon'] = polygon
+		else:
+			geodict[id]['type'] = 'MultiPolygon'
+			geodict[id]['polygons'] = []
+			for polyCoors in p['geometry']['coordinates']:
+				polygonlist = []
+				for coor in polyCoors[0]:
+					polygonlist.append((coor[0],coor[1]))
+				geodict[id]['polygons'].append(Polygon(polygonlist))
+				
 # Import twitter data file into CouchDB
 counter = 1
 with open(melb_tweets_file, encoding="utf-8") as jsonfile:
@@ -50,6 +78,41 @@ with open(melb_tweets_file, encoding="utf-8") as jsonfile:
 				tweet_doc['sentiment_positive'] = sentiment_positive   
 				tweet_doc['sentiment_polarity'] = sentimentPolarity				
 
-				database.save(tweet_doc)
-			
+				# Get Suburb from tweet cordinates
+				try:
+					docCoordinates = tweet_doc['coordinates']
+					try:
+						if (docCoordinates is not None):
+							longitude = docCoordinates['coordinates'][0]
+							latitude = docCoordinates['coordinates'][1]
+							point = Point(longitude, latitude)
+							for i in geodict.keys():
+								if geodict[i]['type'] == 'polygon':
+									if geodict[i]['polygon'].contains(point):
+										suburbId = i
+										suburb = geodict[suburbId]['name']
+										break
+								else:
+									for polygon in geodict[i]['polygons']:
+										if polygon.contains(point):
+											suburbId = i
+											suburb = geodict[suburbId]['name']
+											break
+						else:
+							suburbId = None
+							suburb = None
+					except Exception:
+						suburb = None
+
+					tweet_doc['suburb_id']   = suburbId
+					tweet_doc['suburb_name'] = suburb
+
+					database.save(tweet_doc)
+				
+				except Exception as e:
+					if hasattr(e, 'message'):
+						print(e.message)
+					else:
+						print(e)
+						
 		counter = counter + 1
